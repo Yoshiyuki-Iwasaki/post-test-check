76d1aa857dcd6837ee14dda0ba2ae81479862136
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-01-15 12:04:29 +0900

    create layout component.

---
+++ src/Layout.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Main = styled.div`
+  background: #282c34;
+  height: 100vh;
+`;
+const Inner = styled.div`
+  margin: 0 auto;
+  max-width: 1000px;
+  position: relative;
+`;
+
+const Title = styled.h1`
+  padding-top: 20px;
+  text-align: center;
+  font-size: 22px;
+  color: #fff;
+`;
+
+const Content = styled.div``;
+
+const Layout = ({ children }:any) => {
+  return (
+    <div>
+      <Main>
+        <Inner>
+          <Title>Post-Test-Check</Title>
+          <Content>{children}</Content>
+        </Inner>
+      </Main>
+    </div>
+  );
+};
+
+export default Layout
