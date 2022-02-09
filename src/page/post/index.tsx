4dd5e4964b7a37c7da865f3b928ac5b6c16be5d7
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-09 23:36:29 +0900

    create post page.

---
+++ src/page/post/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PostItem from '../../components/molecules/postItem';
+import data from '../../postData.json';
+import { Content } from './style';
+
+const Post = () => {
+  return (
+    <>
+      <Content>
+        <PostItem data={data[0]} />
+      </Content>
+    </>
+  );
+};
+
+export default Post;
