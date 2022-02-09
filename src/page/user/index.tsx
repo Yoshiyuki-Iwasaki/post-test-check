05450e49dde4bde00a9fbe14e44e33178e4a581e
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-09 23:35:37 +0900

    create user page.

--- src/page/user/index.tsx
+++ src/page/user/index.tsx
@@ -1,14 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import data from '../userData.json';
-import styled from 'styled-components';
-import UserDetail from '../components/organisms/profile/Profile';
-
-const Main = styled.div`
-  margin: 0 auto;
-  padding: 10px;
-  width: 200px;
-  background: #fff;
-`;
+import data from '../../userData.json';
+import UserDetail from '../../components/organisms/profile';
+import { Main } from './style';
 
 const User = () => {
   const [userDate, setUserDate] = useState<any>();
---
+++ src/page/user/style.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+  margin: 0 auto;
+  padding: 10px;
+  width: 200px;
+  background: #fff;
+`;
