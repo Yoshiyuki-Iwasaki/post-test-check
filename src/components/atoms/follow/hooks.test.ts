5ddefcad838d23d78b1733284d1e7859b2404199
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-16 22:32:25 +0900

    add hooks test.

--- src/components/atoms/follow/hooks.test.ts
+++ src/components/atoms/follow/hooks.test.ts
@@ -1,12 +1,16 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import { useFollow } from './hooks';
 
-test('should follow', () => {
+test('挙動に問題ないか', () => {
   const { result } = renderHook(() => useFollow());
 
   act(() => {
     result.current.handleFollow();
   });
-
   expect(result.current.followState).toBe(true);
+
+  act(() => {
+    result.current.handleFollow();
+  });
+  expect(result.current.followState).toBe(false);
 });
---
+++ src/components/atoms/input/hooks.test.ts
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useForm } from './hooks';
+
+test('挙動に問題ないか', () => {
+  const [postData, setPostData] = useState<any>([]);
+  const { result } = renderHook(() => useForm(postData, setPostData));
+  act(() => {
+    result.current.handleInput();
+  });
+  expect(result.current.followState).toBe(true);
+  // act(() => {
+  //   result.current.handleInput();
+  // });
+  // expect(result.current.followState).toBe(false);
+});
