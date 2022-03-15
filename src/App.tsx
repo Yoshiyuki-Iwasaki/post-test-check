6cd17f1f07de63fae14c6b5a25122ffa96aac985
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-03-15 12:01:58 +0900

    fix file directory.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Top from './app/page/top';
-import Post from './app/page/post';
-import User from './app/page/user';
+import Top from './app/page/topPage';
+import Post from './app/page/postPage';
+import User from './app/page/userPage';
 import Layout from './app/components/templates/layout';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
